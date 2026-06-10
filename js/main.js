244f6ea16a3125171cd362f3fc58689e3f7bb5d2
m-nidensi <m_nidensi@cu.edu.ge>
2026-06-10 12:41:24 +0400

    added cards to the page and designed them. also fixed some layout on home page and added 2 @media breakpoint. added aditional efects/design to cards

--- js/main.js
+++ js/main.js
@@ -11,5 +11,44 @@ form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const searchInput = document.getElementById("search-input");
     const games = await searchGames(searchInput.value);
-    console.log(games);
+    renderGames(games);
 });
+
+// functon to desplay the games cards
+function renderGames(games) {
+    const container = document.getElementById("games-container");
+    // clearing the container
+    container.innerHTML = "";
+
+    games.forEach((game) => {
+        const card = document.createElement("div");
+        card.classList.add("game-card");
+
+        // getting an image
+        const image = document.createElement("img");
+        image.src = game.background_image;
+        // in case image wasnt found it will show games name
+        image.alt = game.name;
+        // tital
+        const title = document.createElement("h3");
+        title.textContent = game.name;
+        // rating 
+        const rating = document.createElement("p");
+        rating.textContent = `Rating: ${game.rating}`;
+        // ganras
+        // combine all genre names into one string
+        const genres = document.createElement("p");
+        genres.textContent =
+            "Genres: " +
+            game.genres.map((genre) => genre.name).join(", ")
+
+        // add everything to the created card.
+        card.appendChild(image);
+        card.appendChild(title);
+        card.appendChild(rating);
+        card.appendChild(genres);
+
+        container.appendChild(card);
+
+    });
+}
